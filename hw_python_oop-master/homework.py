f0d60c321e21826cba654d12c517a2d90760d78d
Anton-1991128 <103500567+Anton-1991128@users.noreply.github.com>
2022-05-05 18:51:59 +0300

    текст комментария

--- hw_python_oop-master/homework.py
+++ hw_python_oop-master/homework.py
@@ -66,7 +66,6 @@ class Running(Training):
                  weight: float
                  ) -> None:
         super().__init__(action, duration, weight)
-
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         Running_date = (self.coeff_calorie_1 * self.get_mean_speed() - self.coeff_calorie_2) * self.weight / self.M_IN_KM * self.duration*60
